f8b9f429a3907a0294a936b8c6f7764e5533dbbb
HIRWA Terry <hirwa.terry@gmail.com>
2024-12-17 09:21:57 +0200

    navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";  // Import About component
-import NoPage from "./pages/NoPage"; // Import NoPage component
+import About from "./pages/About";
+import NoPage from "./pages/NoPage";
 
 const App = () => {
   return (
-    <div>
+    <div className="min-h-screen bg-neutral-900">
       <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />    {/* About Page Route */}
-          <Route path="*" element={<NoPage />} />       {/* NoPage */}
-        </Routes>
+        <nav className="bg-neutral-800 p-4">
+          <div className="container mx-auto flex justify-between items-center">
+            <div className="text-orange-500 text-xl font-bold">Trivia Game</div>
+            <ul className="flex space-x-6">
+              <li>
+                <Link to="/home" className="text-white hover:text-orange-500 transition">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/about" className="text-white hover:text-orange-500 transition">
+                  About
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <main className="container mx-auto px-4">
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<NoPage />} />
+          </Routes>
+        </main>
       </BrowserRouter>
     </div>
   );
